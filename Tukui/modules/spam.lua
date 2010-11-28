b61a37d436cb5bc64dccb2cfe5835a51275ceb30
Exx <sergei@madmasters.pri.ee>
2010-11-28 12:38:55 +0200

    Some spam filters

--- Tukui/modules/spam.lua
+++ Tukui/modules/spam.lua
@@ -1,4 +1,4 @@
--- This file is used for enGB or enUS client only.
+﻿-- This file is used for enGB or enUS client only.
 -- translate or do anything you want if you want to 
 -- use this feature on others clients.
 
@@ -74,3 +74,23 @@ local function SPELL_FILTER(self, event, arg1)
     end
 end
 ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", SPELL_FILTER)
+
+----------------------------------------------------------------------------------
+-- I hate mages after 4.0.3a...
+----------------------------------------------------------------------------------
+local function MAGE_FILTER(self, event, arg1)
+	if strfind(arg1,"портал") or strfind(arg1,"порталы") or strfind(arg1, "Порталы") or strfind(arg1, "ПОРТАЛ") or strfind(arg1, "ПОРТАЛЫ") then
+		return true
+	end
+end
+ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", MAGE_FILTER)
+
+----------------------------------------------------------------------------------
+-- Raid Slack Check... спамим в ответ
+----------------------------------------------------------------------------------
+local function RSC_FILTER(self, event, arg1, arg2)
+	if strfind(arg1,"RSC") then
+		SendChatMessage("RaidSlackCheck тащит!", "WHISPER", nil, arg2)
+	end
+end
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", RSC_FILTER)
